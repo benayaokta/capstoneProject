834984be0df00bac6a1f2570e0d5ef4c3bd62613
Benaya Oktavianus <53290795+benayaokta@users.noreply.github.com>
2022-12-08 22:35:09 +0700

    refactor network service

--- CapstoneProject/Service/NetworkService.swift
+++ CapstoneProject/Service/NetworkService.swift
@@ -12,32 +12,20 @@ import Combine
 final class NetworkService: NetworkProtocol {
     init() { }
     
-    func request<T>(config: APIConfiguration, object: T.Type, completion: @escaping (T?, AFError?) -> Void) where T : Decodable, T : Encodable {
+    func request<T>(config: APIConfiguration,
+                    object: T.Type,
+                    completion: @escaping (T?, AFError?) -> Void) where T : Decodable, T : Encodable {
         AF.request(config)
             .validate()
             .responseDecodable(of: T.self,
                                queue: DispatchQueue.global(qos: .background)) { response in
-            switch response.result {
-            case .success(let value):
-                completion(value, nil)
-            case .failure(let error):
-                completion(nil, error)
+                switch response.result {
+                case .success(let value):
+                    completion(value, nil)
+                case .failure(let error):
+                    completion(nil, error)
+                }
             }
-        }
     }
-    
-//    func request<T: Codable>(config: APIConfiguration, object: T.Type) {
-//        AF.request(config)
-//            .validate()
-//            .responseDecodable(of: T.self,
-//                               queue: DispatchQueue.global(qos: .background)) { response in
-//            switch response.result {
-//            case .success(let value):
-//                print("sukses \(value)")
-//            case .failure(let error):
-//                print("error \(error)")
-//            }
-//        }
-//    }
 
 }
