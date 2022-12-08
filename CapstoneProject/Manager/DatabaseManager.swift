4160c76f5d2af48dab0dfadef1cf7c135198f2ce
Benaya Oktavianus <53290795+benayaokta@users.noreply.github.com>
2022-12-08 22:33:50 +0700

    refactor database manager

--- CapstoneProject/Manager/DatabaseManager.swift
+++ CapstoneProject/Manager/DatabaseManager.swift
@@ -20,62 +20,22 @@ final class DatabaseManager {
         userDefault = UserDefaults.init(suiteName: "CapstoneProject")!
     }
     
-//    func getFavoriteList() -> [AllPairs]? {
-//        if let object = userDefault.object(forKey: "favorite") as? [AllPairs] {
-//            return object
-//        } else {
-//            return nil
-//        }
-//    }
-//    
-//    func setFavorite(pair: AllPairs, completion: @escaping (Bool?, AllPairError?) -> Void ) {
-//        if var favorite: [AllPairs] = getFavoriteList() {
-//            if favorite.contains(where: {$0.coinID == pair.coinID}) {
-//                completion(false, .duplicateItem)
-//                return
-//            } else {
-//                favorite.append(pair)
-//            }
-//            userDefault.set(favorite, forKey: "favorite")
-//            completion(true, nil)
-//        } else {
-//            userDefault.set(pair, forKey: "favorite")
-//            completion(true, nil)
-//        }
-//    }
-//    
-//    func deleteFavorite(pair: AllPairs, completion: @escaping (Bool?, AllPairError?) -> Void) {
-//        if var favorite = getFavoriteList() {
-//            if let favoriteIndex = favorite.firstIndex(where: {$0.coinID == pair.coinID}) {
-//                favorite.remove(at: favoriteIndex)
-//                userDefault.set(favorite, forKey: "favorite")
-//                completion(true, nil)
-//            } else {
-//                completion(false, .noPair)
-//                return
-//            }
-//        }
-//    }
-//    
-//    func deleteAllFavorite() {
-//        userDefault.removeObject(forKey: "favorite")
-//    }
-    
     func setData<T: Codable>(value: T, key: String) {
-            let encoder: JSONEncoder = JSONEncoder()
-            if let encoded = try? encoder.encode(value) {
-                userDefault.set(encoded, forKey: key)
-            }
+        let encoder: JSONEncoder = JSONEncoder()
+        if let encoded = try? encoder.encode(value) {
+            userDefault.set(encoded, forKey: key)
         }
+    }
+    
     func getData<T: Codable>(type: T.Type, forKey: String) -> T? {
-            if let data = userDefault.object(forKey: forKey) as? Data {
-                let decoder: JSONDecoder = JSONDecoder()
-                if let value = try? decoder.decode(T.self, from: data) {
-                    return value
-                } else {
-                    return nil
-                }
+        if let data = userDefault.object(forKey: forKey) as? Data {
+            let decoder: JSONDecoder = JSONDecoder()
+            if let value = try? decoder.decode(T.self, from: data) {
+                return value
+            } else {
+                return nil
             }
-            return nil
         }
+        return nil
+    }
 }
